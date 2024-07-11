bc879b6c881a3fd72a5c73ff8a0d24abef533c92
HennrikeSocher <socherhennrike@gmail.com>
2024-07-11 17:34:55 -0300

    Modal complete :rocket:

--- src/app.tsx
+++ src/app.tsx
@@ -5,6 +5,8 @@ import {
   UserRoundPlus,
   Settings2,
   X,
+  AtSign,
+  Plus,
 } from "lucide-react"
 import { useState } from "react"
 
@@ -120,7 +122,7 @@ export function App() {
 
       {isGuestsModalOpen && (
         <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900">
+          <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
             <div className="space-y-2">
               <div className="flex items-center justify-between">
                 <h2> Selecionar convidados</h2>
@@ -133,6 +135,32 @@ export function App() {
                 na viagem.
               </p>
             </div>
+            <div className="flex flex-wrap gap-2">
+              <div className="py-1.5 px-2.5 rounded-md bg-zinc-800 flex items-center gap-2">
+                <span className="text-zinc-300">hennrikesocher@gmail.com</span>
+                <button type="button">
+                  <X className="size-4 text-zinc-400" />
+                </button>
+              </div>
+            </div>
+            <div className="w-full h-px bg-zinc-800" />
+            <form
+              action=""
+              className="py-2.5 bg-zinc-950 border-l-zinc-800 rounded-lg flex items-center gap-2"
+            >
+              <div className="px-2 flex items-center flex-1">
+                <AtSign className="size-5 text-zinc-400" />
+                <input
+                  type="text"
+                  placeholder="Digite o email do convidado"
+                  className="bg-transparent text-lg placeholder:bg-zinc-400 outline-none flex-1"
+                />
+              </div>
+              <button className="bg-lime-300 text-lime-950 px-5 py-2 rounded-lg font-medium flex items-center gap-2 hover:bg-lime-400">
+                Confirmar
+                <Plus className="size-5" />
+              </button>
+            </form>
           </div>
         </div>
       )}
